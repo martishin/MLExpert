5e5a3404846b3b731fcbea9032c76507c8b36d35
Alex Martishin <asmartishin@gmail.com>
2024-01-17 22:28:32 +0100

    Solved the k nearest problem

---
+++ src/model_concepts/k_nearest_neighbors/solution.py
@@ -0,0 +1,37 @@
+import math
+from typing import List, TypedDict, Dict
+
+
+class Feature(TypedDict):
+    features: List[float]
+    is_intrusive: int
+
+
+def predict_label(
+    examples: Dict[str, Feature],
+    features: List[float],
+    k: int,
+    label_key: str = "is_intrusive",
+) -> int:
+    k_nearest_neighbors = find_k_nearest_neighbors(examples, features, k)
+    k_nearest_neighbors_labels = [
+        examples[pid][label_key] for pid in k_nearest_neighbors
+    ]
+    return round(sum(k_nearest_neighbors_labels) / k)
+
+
+def find_k_nearest_neighbors(
+    examples: Dict[str, Feature], features: List[float], k: int
+) -> List[str]:
+    distances: Dict[str, float] = {}
+    for pid, features_label_map in examples.items():
+        distance = get_euclidean_distance(features, features_label_map["features"])
+        distances[pid] = distance
+    return sorted(distances, key=distances.get)[:k]
+
+
+def get_euclidean_distance(features: List[float], other_features: List[float]) -> float:
+    squared_differences = []
+    for i in range(len(features)):
+        squared_differences.append((other_features[i] - features[i]) ** 2)
+    return math.sqrt(sum(squared_differences))
---
+++ src/model_concepts/k_nearest_neighbors/tests.py
@@ -0,0 +1,102 @@
+import json
+
+from solution import find_k_nearest_neighbors, predict_label
+import unittest
+
+
+def get_knn_examples():
+    with open("./data.json", "rb") as handle:
+        return json.load(handle)
+
+
+EXAMPLES = get_knn_examples()
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 1
+        expected = ["pid_500"]
+        actual = find_k_nearest_neighbors(EXAMPLES, features, k)
+        self.assertEqual(sorted(actual), sorted(expected))
+
+    def test_case_2(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 1
+        expected = 1
+        actual = predict_label(EXAMPLES, features, k)
+        self.assertEqual(actual, expected)
+
+    def test_case_3(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 3
+        expected = ["pid_500", "pid_535", "pid_545"]
+        actual = find_k_nearest_neighbors(EXAMPLES, features, k)
+        self.assertEqual(sorted(actual), sorted(expected))
+
+    def test_case_4(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 3
+        expected = 1
+        actual = predict_label(EXAMPLES, features, k)
+        self.assertEqual(actual, expected)
+
+    def test_case_5(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 5
+        expected = ["pid_500", "pid_535", "pid_545", "pid_512", "pid_516"]
+        actual = find_k_nearest_neighbors(EXAMPLES, features, k)
+        self.assertEqual(sorted(actual), sorted(expected))
+
+    def test_case_6(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 5
+        expected = 1
+        actual = predict_label(EXAMPLES, features, k)
+        self.assertEqual(actual, expected)
+
+    def test_case_7(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 7
+        expected = [
+            "pid_500",
+            "pid_535",
+            "pid_545",
+            "pid_512",
+            "pid_516",
+            "pid_513",
+            "pid_537",
+        ]
+        actual = find_k_nearest_neighbors(EXAMPLES, features, k)
+        self.assertEqual(sorted(actual), sorted(expected))
+
+    def test_case_8(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 7
+        expected = 1
+        actual = predict_label(EXAMPLES, features, k)
+        self.assertEqual(actual, expected)
+
+    def test_case_9(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 9
+        expected = [
+            "pid_500",
+            "pid_535",
+            "pid_545",
+            "pid_512",
+            "pid_516",
+            "pid_513",
+            "pid_537",
+            "pid_540",
+            "pid_528",
+        ]
+        actual = find_k_nearest_neighbors(EXAMPLES, features, k)
+        self.assertEqual(sorted(actual), sorted(expected))
+
+    def test_case_10(self):
+        features = [4.30936122, 4.28739283, 4.29680938, 4.33571647, 4.28774593]
+        k = 9
+        expected = 0
+        actual = predict_label(EXAMPLES, features, k)
+        self.assertEqual(actual, expected)
